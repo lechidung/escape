609bd50d2e1e902d86bc80d199b97d30371bbe1e
Dung (Johnny) <lechidungvl@hotmail.com>
2020-12-08 00:38:54 +0700

    Update to test lib

--- html.ts
+++ html.ts
@@ -6,6 +6,16 @@
 const matchEscHtmlRegExp = /["'&<>]/;
 const matchUnEscRegExp = /&(?:amp|#38|lt|#60|gt|#62|apos|#39|quot|#34);/g;
 
+export function isEscape(str: string): boolean {
+  const matchEscHtml = matchEscHtmlRegExp.exec(str);
+
+  if (!matchEscHtml) {
+    return false;
+  }
+
+  return true;
+}
+
 export function escapeHtml(str: string): string {
   const matchEscHtml = matchEscHtmlRegExp.exec(str);
   if (!matchEscHtml) {
@@ -45,4 +55,29 @@ export function escapeHtml(str: string): string {
   }
 
   return lastIndex !== index ? html + str.substring(lastIndex, index) : html;
+}
+
+export function isUnescape(str: string): boolean {
+  const matchUnEsc = matchUnEscRegExp.exec(str);
+  if (!matchUnEsc) {
+    return false;
+  }
+
+  return true;
+}
+
+export function unescapeHtml(str: string): string {
+  const matchUnEsc = matchUnEscRegExp.exec(str);
+  if (!matchUnEsc) {
+    return str;
+  }
+
+  const res = str.replace(/&quot;/g, '"')
+    .replace(/&#39;/g, '\'')
+    .replace(/&#x3A;/g, ':')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&amp;/g, '&');
+
+  return unescapeHtml(res);
 }
